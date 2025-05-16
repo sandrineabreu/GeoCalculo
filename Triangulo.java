6fb5f46c5675dab5662053b86c51810066b6b0f2
sandrine <sandrine.abreu03@aluno.ifce.edu.br>
2025-05-16 15:09:01 -0300

    Update Triangulo.java

--- Triangulo.java
+++ Triangulo.java
@@ -3,8 +3,27 @@ public class Triangulo() {
 	private double base;
 	private double altura;
 	private double area;
-	public
-
+	public Triangulo() {
+		base = 0;
+		altura = 0;
+		area = 0;
+		
+	public void recebeAltura(double altura){
+		this.altura = altura;
 	}
-
+	public void recebeBase(double pBase){
+		this.base = pBase;
+	
+	}
+	
+	public void calcArea(){
+		area = area;
+	}
+	
+	public void mostrarArea(){
+		System.out.println(area);
+	}
+	
+	
 }
+
