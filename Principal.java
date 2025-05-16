700dc931ded4edfc7b5bc4e8577335094c357624
sandrine <sandrine.abreu03@aluno.ifce.edu.br>
2025-05-16 15:13:43 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -12,8 +12,26 @@ public class Principal {
 		fg1.calcArea();
 		fg1.mostrarArea();
 		double recebeArea = fg1.mostrarAreacomRetorno();
-	
+
+
+				
+		Quadrado fg2 = new Quadrado();
+		fg2.recebeLado(8);
+		fg2.calcArea();
+		fg2.mostrarArea();
+		recebeArea = fg2.mostrarAreaComRetorno();
 		
+		Circulo fg3 = new Circulo();
+		fg3.recebaRaio(9);
+		fg3.calcArea();
+		fg3.mostrarArea(); 
+		recebeArea= fg3.mostrarAreaComRetorno();
+		
+		Hexagono fg9= new Hexagono();
+		fg9.recebaLado(4);
+		fg9.calcArea();
+		fg9.mostrarArea();
+		recebeArea = fg9.mostrarAreaComRetorno();
 		
 	}
 	
